719911cc61304d643b79cb174e4788a27f4a676e
jayant sarkar <codejayant@gmail.com>
2020-07-27 01:37:53 -0700

    Associate route table to subnets

--- vpc_networking/main.tf
+++ vpc_networking/main.tf
@@ -86,3 +86,33 @@ resource "aws_route_table" "private_route_table" {
     Name="Private-Route-Table"
   }
 }
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id = aws_subnet.module_public_subnet_1.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id = aws_subnet.module_public_subnet_2.id
+}
+
+resource "aws_route_table_association" "public_subnet_3_association" {
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id = aws_subnet.module_public_subnet_3.id
+}
+
+resource "aws_route_table_association" "private_subnet_1_association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id = aws_subnet.module_private_subnet_1.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id = aws_subnet.module_private_subnet_2.id
+}
+
+resource "aws_route_table_association" "private_subnet_3_association" {
+  route_table_id = aws_route_table.private_route_table.id
+  subnet_id = aws_subnet.module_private_subnet_3.id
+}

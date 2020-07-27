e8cb395c0278708499f457c5acc8675f27ded5de
jayant sarkar <codejayant@gmail.com>
2020-07-27 01:52:07 -0700

    Create an Internet Gateway and add to Route Table

--- vpc_networking/main.tf
+++ vpc_networking/main.tf
@@ -140,3 +140,17 @@ resource "aws_route" "nat_gateway_route" {
   nat_gateway_id = aws_nat_gateway.nat_gateway.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.module_vpc.id
+
+  tags = {
+    Name="Production-IGW"
+  }
+}
+
+resource "aws_route" "igw_route" {
+  route_table_id = aws_route_table.public_route_table.id
+  gateway_id = aws_internet_gateway.internet_gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
